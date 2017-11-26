adecab429a6d334c6422acdb74faadd9f0305554
neelbhuva <[neel.7365@gmail.com]>
2017-11-25 23:36:20 -0500

    bigram count matrix

--- Project/ngram.py
+++ Project/ngram.py
@@ -2,22 +2,30 @@ import math
 import numpy as np
 from nltk import ngrams
 import regex as re
+import pandas as pd
+from collections import Counter
+import itertools
 
 class Ngram():
 	#create variables here to share among all instances of this class
 	def __init__(self,fd):
 		self.entire_text = fd.read()
 		self.n_grams = []
+		self.n_grams_count = []
+		self.bigram_df = pd.DataFrame()	#bigram counts/prob matrix
+		self.unigram_count = {}
+		self.V = 0	# number of words in vocabulary
+		self.N = 0	#total number of words
 
 	def readNGrams(self,n):
 		self.n_grams = ngrams(self.entire_text.split(), n)
 		self.n_grams = list(self.n_grams)
 
 	def printNGrams(self,n):
-		n_gram_count = [(item, self.n_grams.count(item)) for item in sorted(set(self.n_grams))]
-		print(n_gram_count[:n])
-		# for i in self.n_grams[:n]:
-		# 	print(i)
+		print(self.n_grams_count[:n])
+
+	def getCounts(self):
+		self.n_grams_count = [(item, self.n_grams.count(item)) for item in sorted(set(self.n_grams))]
 
 	def getSpecifiedMatches(self,match):
 		pattern = re.compile(match)
@@ -71,6 +79,8 @@ class Ngram():
 		inside_paranthesis = pattern.findall(self.entire_text)
 		#print(inside_paranthesis)
 		self.entire_text = pattern.sub(" ",self.entire_text)
+		#single parantheses
+		self.entire_text = re.sub("(\s)+\((\w)+|(\s)+(\w)+\(| \( |(\s)+\)(\w)+|(\s)+(\w)+\)| \) "," ",self.entire_text)
 		#add end token at the end of sentences
 		pattern = re.compile("\.(\s)*|[!]+(\s)*|(\?)+(\s)*|\)(\s)+",re.IGNORECASE)
 		self.entire_text = pattern.sub(end_token,self.entire_text)
@@ -84,17 +94,78 @@ class Ngram():
 		for i in temp:
 			self.n_grams.remove(i)
 
-if __name__ == '__main__':
-	fd = open("/home/neel/ngram data/en_US/blogs_test.txt")
-	#fd = open("/home/neel/ngram data/en_US/en_US.blogs.txt")
-	#number of grams to be fetched from the text file
-	n = 3
-	start_token = "<s> "*n
-	x = Ngram(fd)
+	def saveNGramsCountToFile(self,file):
+		with open(file, 'w') as fp:
+			for x in self.n_grams_count:
+				s = ""
+				for i in x[0]:	
+					s = s + i + " "
+				fp.write('{} {}\n'.format(s,x[1]))
+		fp.close()
+
+	def getUnigramCountAndInitializeBigramMatrix(self):
+		wordList = self.entire_text.split()
+		self.unigram_count = Counter(wordList)
+		#keywords = [' '.join(i) for i in itertools.product(wordList, repeat = 3)]
+		self.N = len(wordList)
+		wordList = list(set(wordList))
+		self.V = len(wordList)
+		col1 = "WORDS_LIST"
+		wordList.insert(0,col1)
+		data = np.zeros((len(wordList)-1,len(wordList)))
+		self.bigram_df = pd.DataFrame(data=data,columns=wordList)
+		self.bigram_df[col1] = wordList[1:len(wordList)]
+		self.bigram_df = self.bigram_df.set_index(col1)
+		#print(keywords)
+
+	def getCount(self,a,b):
+		my_regex = re.escape(a) + "(\s)+" + re.escape(b)
+		pat = re.compile(my_regex)
+		m = pat.findall(self.entire_text)
+		return m
+		# t = a + " " + b
+		# for i in self.n_grams_count:
+		# 	u = i[0][0]
+		# 	v = i[0][1]
+		# 	s = ' '.join(i[0])
+
+	def populateProbMatrix(self):
+		wl = self.entire_text.split()
+		for i in range(0,len(wl)-1):
+			a = wl[i]
+			b = wl[i+1]
+			#print(a,b,self.bigram_df[a][b],end = "||||")
+			self.bigram_df[a][b] = self.bigram_df[a][b] + 1
+		#print(self.bigram_df["makes"]["them"])
+		#print(wl)
+		# cols = self.bigram_df.columns
+		# for i in cols:
+		# 	for j in cols:
+		# 		df[i][j] = self.getCount(j,i)
+
+def cleanAndGetCounts(x):
 	#x.getSpecifiedMatches("(jan|feb|mar|apr|may|jun|jul|aug|sept|oct|nov|dec)(\.)?(\s)?(\d){1,2}")
 	x.cleanAndInsertEndToken(" </s> "+start_token)
+	fd.close()
 	x.readNGrams(n)
 	#number of ngrams to print
 	x.delNGramsWithEndTokenInWrongPlace()
+	x.getCounts()
 	p = 100
-	x.printNGrams(p)
+	#x.printNGrams(p)
+	x.saveNGramsCountToFile("blogs_count_bigram.txt")
+
+if __name__ == '__main__':
+	fd = open("/home/neel/ngram data/en_US/blogs_test2.txt")
+	#fd = open("/home/neel/ngram data/en_US/en_US.blogs.txt")
+	#number of grams to be fetched from the text file
+	n = 2
+	start_token = "<s> "*n
+	x = Ngram(fd)
+	cleanAndGetCounts(x)
+	x.getUnigramCountAndInitializeBigramMatrix()
+	x.populateProbMatrix()
+	# a = ["neel","am","cool","bro"]
+	# keywords = [' '.join(i) for i in itertools.product(a, repeat = 3)]
+	# print(keywords)
+
